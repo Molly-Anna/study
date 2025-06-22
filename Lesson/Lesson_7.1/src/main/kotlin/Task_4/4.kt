b45eeebcd3c58d302bb8b717b044365efa919fc0
Anna Chirkova <kawabangaja@mail.ru>
2025-06-22 13:25:38 +0300

    Lesson_7/1/2/3/4/5

--- Lesson/Lesson_7.1/src/main/kotlin/Task_4/4.kt
+++ Lesson/Lesson_7.1/src/main/kotlin/Task_4/4.kt
@@ -14,17 +14,15 @@ fun main() {
 
 /**
  * Функция меняет первую и последнюю букву каждого слова.
- * возвращает строку разбитую на слова пробелами,
- *     после обработки объединяю слова пробелами,
- *     если в слове больше 1 символа где первый и последний символ буквы,
- *     меняю местами первую и последнюю букву
+ * @return возвращает строку разбитую на слова пробелами, после обработки объединяю слова пробелами,
+ * если в слове больше 1 символа где первый и последний символ буквы, меняю местами первую и последнюю букву
  *
- *     Или
- *  * fun swapLetter2(str: String) :String {
- *  *     return Regex("""\b(\w)(\w*)(\w)\b""").replace(str) {
- *  *         "${it.groupValues[3]}${it.groupValues[2]}${it.groupValues[1]}"
- *  *     }
- *  * }
+ * Или
+ *  @param fun swapLetter2(str: String) :String {
+ *  return Regex("""\b(\w)(\w*)(\w)\b""").replace(str) {
+ * "${it.groupValues[3]}${it.groupValues[2]}${it.groupValues[1]}"
+ *  }
+ *  }
  */
 fun swapLetter(str: String): String {
     return str.split(" ").joinToString(" ") { word ->
